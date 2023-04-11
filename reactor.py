67bfed7aed97975fd28facbe12000221f8adca5d
Changhae Andrew Kim <changhae_kim@outlook.com>
2023-04-11 11:07:00 -0500

    rename _log_ --> _log_n_; added get_..._partition which recalculates alpha1m, not yet used in population balance

--- reactor.py
+++ reactor.py
@@ -1,24 +1,30 @@
 import numpy
 
 from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
 
 
 class BatchReactor():
 
     def __init__( self, nmax=5, grid=0, mode='discrete',
             rho=None, alpha1m=None, H0=None, H1=None,
-            concs=[0.0, 0.0, 0.0, 0.0, 1.0],
-            temp=573.15, volume=1.0, mass=10.0, monomer=14.027 ):
+            randomness=0.0, concs=[0.0, 0.0, 0.0, 0.0, 1.0],
+            temp=573.15, volume=1.0, mass=10.0, monomer=14.027, dens=920.0 ):
 
         if mode == 'discrete':
             self.n = numpy.arange(1, nmax+1, 1)
+            self.get_partition = self.get_discrete_partition
             self.get_rate = self.get_discrete_rate
         elif mode == 'continuum':
             self.n = numpy.linspace(1.0, nmax, grid)
+            self.get_partition = self.get_continuum_partition
             self.get_rate = self.get_continuum_rate
-        elif mode == 'log':
+        elif mode == 'log_n':
             self.n = numpy.logspace(0.0, numpy.log10(nmax), grid)
-            self.get_rate = self.get_log_rate
+            self.get_partition = self.get_log_n_partition
+            self.get_rate = self.get_log_n_rate
+
+        self.randomness = randomness
 
         if rho is None:
             n = self.n
@@ -30,13 +36,13 @@ class BatchReactor():
                 dn = n[1] - n[0]
                 w = numpy.ones_like(n)
                 w[0] = w[-1] = 0.5
-                rho = numpy.array(concs) / numpy.einsum('i,i,i->', w, n, concs) * dn
-            elif mode == 'log':
-                x = numpy.log(n)
-                dx = x[1] - x[0]
+                rho = numpy.array(concs) / ( numpy.einsum('i,i,i->', w, n, concs) * dn )
+            elif mode == 'log_n':
+                r = numpy.log(n)
+                dr = r[1] - r[0]
                 w = numpy.ones_like(n)
                 w[0] = w[-1] = 0.5
-                rho = numpy.array(concs) / numpy.einsum('i,i,i->', w, concs, numpy.exp(2.0*x)) * dx
+                rho = numpy.array(concs) / ( numpy.einsum('i,i,i->', w, concs, numpy.exp(2.0*r)) * dr )
         self.rho = rho
 
         if alpha1m is None:
@@ -46,11 +52,98 @@ class BatchReactor():
             if H1 is None:
                 H1 = numpy.exp( 3.31727830327662E-01 - 5.34127400185307E+02 / temp )
             alpha1m = 1.0 / ( 1.0 + n * H0 * H1**n )
+        self.rho_M = dens / ( mass / volume )
+        self.H0 = H0
+        self.H1 = H1
         self.alpha1m = alpha1m
 
         return
 
-    def get_discrete_rate( self, n=None, rho=None, alpha1m=None ):
+    def get_discrete_partition( self, n=None, rho=None, rho_M=None, H0=None, H1=None, gtol=1e-6 ):
+
+        if n is None:
+            n = self.n
+        if rho is None:
+            rho = self.rho
+        if rho_M is None:
+            rho_M = self.rho_M
+        if H0 is None:
+            H0 = self.H0
+        if H1 is None:
+            H1 = self.H1
+
+        def fun(x):
+            W_M = numpy.einsum('i,i,i->', n, numpy.exp(x), rho)
+            d = 1.0 / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n ) - numpy.exp(x)
+            f = numpy.inner(d, d)
+            dfdx = 2.0 * ( ( n * ( n * numpy.exp(x) * rho / W_M**2 ) * H0 * H1**n ) / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n )**2 - numpy.exp(x) ) * d
+            return f, dfdx
+
+        x = numpy.log( 1.0 / ( 1.0 + n * H0 * H1**n ) )
+        solver = minimize(fun, x, method='BFGS', jac=True, options={'gtol': gtol})
+
+        return numpy.exp(solver.x)
+
+    def get_continuum_partition( self, n=None, rho=None, rho_M=None, H0=None, H1=None, gtol=1e-6 ):
+
+        if n is None:
+            n = self.n
+        if rho is None:
+            rho = self.rho
+        if rho_M is None:
+            rho_M = self.rho_M
+        if H0 is None:
+            H0 = self.H0
+        if H1 is None:
+            H1 = self.H1
+
+        dn = n[1] - n[0]
+        w = numpy.ones_like(n)
+        w[0] = w[-1] = 0.5
+
+        def fun(x):
+            W_M = numpy.einsum('i,i,i,i->', w, n, numpy.exp(x), rho) * dn
+            d = 1.0 / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n ) - numpy.exp(x)
+            f = numpy.inner(d, d)
+            dfdx = 2.0 * ( ( n * ( w * n * numpy.exp(x) * rho * dn / W_M**2 ) * H0 * H1**n ) / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n )**2 - numpy.exp(x) ) * d
+            return f, dfdx
+
+        x = numpy.log( 1.0 / ( 1.0 + n * H0 * H1**n ) )
+        solver = minimize(fun, x, method='BFGS', jac=True, options={'gtol': gtol})
+
+        return numpy.exp(solver.x)
+
+    def get_log_n_partition( self, n=None, rho=None, rho_M=None, H0=None, H1=None, gtol=1e-6 ):
+
+        if n is None:
+            n = self.n
+        if rho is None:
+            rho = self.rho
+        if rho_M is None:
+            rho_M = self.rho_M
+        if H0 is None:
+            H0 = self.H0
+        if H1 is None:
+            H1 = self.H1
+
+        r = numpy.log(n)
+        dr = r[1] - r[0]
+        w = numpy.ones_like(n)
+        w[0] = w[-1] = 0.5
+
+        def fun(x):
+            W_M = numpy.einsum('i,i,i,i->', w, numpy.exp(x), rho, numpy.exp(2.0*r)) * dr
+            d = 1.0 / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n ) - numpy.exp(x)
+            f = numpy.inner(d, d)
+            dfdx = 2.0 * ( ( n * ( w * numpy.exp(x) * rho * numpy.exp(2.0*r) * dr / W_M**2 ) * H0 * H1**n ) / ( 1.0 + n * ( 1.0/W_M - 1.0/rho_M ) * H0 * H1**n )**2 - numpy.exp(x) ) * d
+            return f, dfdx
+
+        x = numpy.log( 1.0 / ( 1.0 + n * H0 * H1**n ) )
+        solver = minimize(fun, x, method='BFGS', jac=True, options={'gtol': gtol})
+
+        return numpy.exp(solver.x)
+
+    def get_discrete_rate( self, n=None, rho=None, alpha1m=None, randomness=None ):
 
         if n is None:
             n = self.n
@@ -58,6 +151,8 @@ class BatchReactor():
             rho = self.rho
         if alpha1m is None:
             alpha1m = self.alpha1m
+        if randomness is None:
+            randomness = self.randomness
 
         y = alpha1m * rho
 
@@ -70,7 +165,7 @@ class BatchReactor():
 
         return rate
 
-    def get_continuum_rate( self, n=None, rho=None, alpha1m=None ):
+    def get_continuum_rate( self, n=None, rho=None, alpha1m=None, randomness=None ):
 
         if n is None:
             n = self.n
@@ -78,6 +173,8 @@ class BatchReactor():
             rho = self.rho
         if alpha1m is None:
             alpha1m = self.alpha1m
+        if randomness is None:
+            randomness = self.randomness
 
         y = alpha1m * rho
         dn = n[1] - n[0]
@@ -96,7 +193,7 @@ class BatchReactor():
 
         return rate
 
-    def get_log_rate( self, n=None, rho=None, alpha1m=None ):
+    def get_log_n_rate( self, n=None, rho=None, alpha1m=None, randomness=None ):
 
         if n is None:
             n = self.n
@@ -104,6 +201,8 @@ class BatchReactor():
             rho = self.rho
         if alpha1m is None:
             alpha1m = self.alpha1m
+        if randomness is None:
+            randomness = self.randomness
 
         y = alpha1m * rho
 
