eddca06c989ec0bc959e524ac750ff0be3ae5253
Changhae Andrew Kim <changhae_kim@outlook.com>
2023-05-08 14:34:12 -0500

    bug fix pre- post-process

--- reactor.py
+++ reactor.py
@@ -359,11 +359,11 @@ class BatchReactor():
             n = x
             rho = y.T / n**2
         elif yscale == 'dWdn':
-            x = n
-            y = mass * n * rho.T
+            n = x
+            rho = y.T / n / mass
         elif yscale == 'dWdlogn':
-            x = n
-            y = mass * n**2 * rho.T
+            n = x
+            rho = y.T / n**2 / mass
         elif yscale == 'dWdM':
             n = x / monomer
             rho = y.T / n * monomer / mass
@@ -389,6 +389,12 @@ class BatchReactor():
         elif yscale == 'dwdlogn':
             x = n
             y = n**2 * rho.T
+        elif yscale == 'dWdn':
+            x = n
+            y = mass * n * rho.T
+        elif yscale == 'dWdlogn':
+            x = n
+            y = mass * n**2 * rho.T
         elif yscale == 'dWdM':
             x = monomer * n
             y = mass / monomer * n * rho.T
