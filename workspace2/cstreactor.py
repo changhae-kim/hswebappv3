57ba5df5dfbdf8946b5c32a1fb1ba42a8fbfa9f3
Changhae Andrew Kim <changhae_kim@outlook.com>
2024-01-23 16:00:26 -0600

    Revert "fix cstr"
    This reverts commit 87b18004b4dfd896438346881cbfcc6ebb71ee7b.

--- workspace2/cstreactor.py
+++ workspace2/cstreactor.py
@@ -4,7 +4,8 @@ import os, sys
 from matplotlib import pyplot
 pyplot.rcParams.update({'font.size': 16})
 
-from reactor import CSTReactor, SemiBatchReactor
+from reactor import CSTReactor
+from reactor import SemiBatchReactor
 from utils import plot_curves, plot_two_axes, plot_populations
 
 
@@ -13,8 +14,8 @@ mass    = 1.0
 volume  = 1.0
 mu      = 3.0
 sigma   = 0.1
-influx  = 0.01
-outflux = 100.0
+influx  = 1.0
+outflux = 1.0
 
 monomer = 14.027
 dens    = 940.0
@@ -27,45 +28,35 @@ nmin = 1e-2
 nmax = 10.0**(mu+4.0*sigma)
 mesh = int(100.0*(numpy.log10(nmax)-numpy.log10(nmin)))
 n = numpy.logspace(numpy.log10(nmin), numpy.log10(nmax), mesh)
+concs = (1.0/n**2)*numpy.exp(-0.5*((numpy.log10(n)-mu)/(sigma))**2)
 tmax = 1.0
 
-logn = numpy.log(n)
-dlogn = logn[1] - logn[0]
-rho = (1.0/n**2)*numpy.exp(-0.5*((numpy.log10(n)-mu)/(sigma))**2)
-g = n**2 * rho
-rho = rho / ( 0.5 * numpy.sum( g[1:] + g[:-1] ) * dlogn )
-concs = ( mass / volume / monomer ) * rho
-inconcs = ( dens / monomer ) * rho
-
-influx = influx*inconcs
+influx = influx*concs
 outflux = [outflux, 0.0]
 
-reactor = CSTReactor(nmin=nmin, nmax=nmax, mesh=mesh, grid=grid, influx=influx, outflux=outflux, concs=concs, temp=temp, volume=volume, mass=mass, monomer=monomer, dens=dens, rand=1.0)
-#reactor = SemiBatchReactor(nmin=nmin, nmax=nmax, mesh=mesh, grid=grid, influx=influx, outflux=outflux, concs=concs, temp=temp, volume=volume, mass=mass, monomer=monomer, dens=dens, rand=1.0)
+reactor = SemiBatchReactor(nmin=nmin, nmax=nmax, mesh=mesh, grid=grid, influx=influx, outflux=outflux, concs=concs, temp=temp, volume=volume, mass=mass, monomer=monomer, dens=dens, rand=1.0)
 n = reactor.n
+#alpha, alpha1m = reactor.get_part()
 reactor.W = None
 reactor.V = None
 reactor.alpha = None
 reactor.alpha1m = None
 
 if not os.path.exists('rho_'+basename+'.npy'):
-    rho = reactor.solve(gtol=1e-12, rtol=1e-12, atol=1e-12)
+    #rho = reactor.solve(gtom=1e-12)
+    t, rho = reactor.solve(tmax, gtol=1e-12, rtol=1e-12, atol=1e-12)
+    numpy.save('t_'+basename+'.npy', t)
     numpy.save('rho_'+basename+'.npy', rho)
-    t = numpy.array([0.0, 0.5, 1.0])
-    rho = numpy.transpose([rho, rho, rho])
-    #t, rho = reactor.solve(tmax, gtol=1e-12, rtol=1e-12, atol=1e-12)
-    #numpy.save('t_'+basename+'.npy', t)
-    #numpy.save('rho_'+basename+'.npy', rho)
 else:
+    t = numpy.load('t_'+basename+'.npy')
     rho = numpy.load('rho_'+basename+'.npy')
-    t = numpy.array([0.0, 0.5, 1.0])
-    rho = numpy.transpose([rho, rho, rho])
-    #t = numpy.load('t_'+basename+'.npy')
-    #rho = numpy.load('rho_'+basename+'.npy')
 
 alpha, alpha1m = reactor.get_part(rho=rho[:, -1])
 
 g, gin, gout, G, Gin, Gout = reactor.cointegrate(t=t, rho=rho, integrals_only=False)
+#rho_reac = ( reactor.rho + ( G + Gin - Gout ).T ).T
+#rho_cond = Gout
+#rho_comb = ( reactor.rho + ( G + Gin ).T ).T
 rho_reac = rho
 rho_cond = gout
 
@@ -78,4 +69,9 @@ n, dwdn_cond = reactor.postprocess('dwdn', t=t, rho=rho_cond)
 n, dwdlogn_cond = reactor.postprocess('dwdlogn', t=t, rho=rho_cond)
 plot_populations(t, n, dwdlogn_cond*numpy.log(10.0), alpha1m, '$n$', r'$d\widetilde{W}/d\log{n}$', 'dwdlogn_cond_'+basename+'.png', xlim=[1.0, nmax])
 plot_populations(t, n, dwdn_cond, alpha1m, '$n$', r'$d\widetilde{W}/d{n}$', 'dwdn_cond_'+basename+'.png', xscale='linear', xlim=[1.0, 29.0])
+
+#n, dwdn_comb = reactor.postprocess('dwdn', t=t, rho=rho_comb)
+#n, dwdlogn_comb = reactor.postprocess('dwdlogn', t=t, rho=rho_comb)
+#plot_populations(t, n, dwdlogn_comb*numpy.log(10.0), alpha1m, '$n$', r'$d\widetilde{W}/d\log{n}$', 'dwdlogn_comb_'+basename+'.png', xlim=[1.0, nmax])
+#plot_populations(t, n, dwdn_comb, alpha1m, '$n$', r'$d\widetilde{W}/d{n}$', 'dwdn_comb_'+basename+'.png', xscale='linear', xlim=[1.0, 29.0])
 
